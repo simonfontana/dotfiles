709106db2d33ee1c889cb83d8fd931d3d41286a7
Simon Fontana Oscarsson <simon.fontana.oscarsson@ericsson.com>
2022-03-22 11:20:49 +0100

    Complete the / in ../

--- .zsh/autocomplete.zsh
+++ .zsh/autocomplete.zsh
@@ -3,7 +3,8 @@ autoload -U compinit
 zstyle ':completion:*' menu select
 zmodload zsh/complist
 compinit
-_comp_options+=(globdots)		# Include hidden files.
+# _comp_options+=(globdots)		# Include hidden files.
+zstyle ':completion:*' special-dirs true
 
 source <(kubectl completion zsh)
 source <(helm completion zsh)
